7c6be6c282fdf244beb5c0da2d6f7498412d3997
disconaked <nolan.klinke@gmail.com>
2019-02-17 18:48:42 -0500

    Updates

---
+++ precache-manifest.89a13c164738f49d90ddef6ff9489fd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/avatarfriends_redux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ee7b1df5a97c0aa7dabb",
+    "url": "/avatarfriends_redux/static/js/runtime~main.ee7b1df5.js"
+  },
+  {
+    "revision": "07f66ead04b410859749",
+    "url": "/avatarfriends_redux/static/js/main.07f66ead.chunk.js"
+  },
+  {
+    "revision": "981cb22c589f0af72625",
+    "url": "/avatarfriends_redux/static/js/1.981cb22c.chunk.js"
+  },
+  {
+    "revision": "07f66ead04b410859749",
+    "url": "/avatarfriends_redux/static/css/main.d27b5ec0.chunk.css"
+  },
+  {
+    "revision": "981cb22c589f0af72625",
+    "url": "/avatarfriends_redux/static/css/1.c4aadd37.chunk.css"
+  },
+  {
+    "revision": "5ad83b277fbf2ced2963ea26ad304eaf",
+    "url": "/avatarfriends_redux/index.html"
+  }
+];
